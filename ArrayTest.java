dd13a9e9166343b1d7e08ce2c0e82f27aa4ca4ff
Kurt <52134197+KurtHilder@users.noreply.github.com>
2020-05-20 13:40:02 +0200

    init upload

---
+++ ArrayTest.java
@@ -0,0 +1,26 @@
+public class ArrayTest{
+   
+   public static void main(String[] args){
+   
+   // arrays are just <lists>.
+   // they are always the same size (amount of elements).
+      
+//       int[] numbers = {10, 20, 30, 40, 50};
+//                        // 0,  1,  2,  3,  4   
+//       
+//       for(int x = 0; x <=4; x++)
+//       {
+//          System.out.println(numbers[x]);
+//       }
+      
+      String[] words = {"Hello", "how", "are", "you", "?"};
+                        // 0        1      2     3
+      for(int x = 0; x <=4; x++){
+      
+         System.out.print(words[x] + " " );
+      }
+      
+      
+   
+   }
+}
